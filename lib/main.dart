789b5073966eda92e454f379ae66c50a667ef037
loloof64 <laurent.bernabe@gmail.com>
2023-07-12 17:27:50 +0200

    showing 'RGPD rules' at start of application

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,7 +10,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -26,21 +26,6 @@ class MyApp extends StatelessWidget {
         Locale.fromSubtags(languageCode: 'fr'),
       ],
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -48,6 +33,9 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+const privacyPolicyUrl = "https://basic-chess-endgames.netlify.app/privacy";
+const useConditionsUrl = "https://basic-chess-endgames.netlify.app/conditions";
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
@@ -57,71 +45,102 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  @override
+  void initState() {
+    WidgetsBinding.instance.addPostFrameCallback((_) => _showRgpdWarning());
+    super.initState();
+  }
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  void _showRgpdWarning() {
+    const height = 200.0;
+    const spacerHeight = 10.0;
+    showModalBottomSheet(
+        isDismissible: false,
+        context: context,
+        builder: (ctx2) {
+          return Container(
+            height: height,
+            color: Colors.grey[200],
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    AppLocalizations.of(context)!.rgpdText,
+                  ),
+                  const SizedBox(
+                    height: spacerHeight,
+                  ),
+                  InkWell(
+                    child: Text(
+                      AppLocalizations.of(context)!.rgpdPrivacy,
+                      style: TextStyle(
+                        color: Colors.blue[300],
+                        decoration: TextDecoration.underline,
+                      ),
+                    ),
+                    onTap: () async {
+                      final uri = Uri.parse(privacyPolicyUrl);
+                      if (await canLaunchUrl(uri)) await launchUrl(uri);
+                    },
+                  ),
+                  const SizedBox(
+                    height: spacerHeight,
+                  ),
+                  InkWell(
+                    child: Text(
+                      AppLocalizations.of(context)!.rgpdUseConditions,
+                      style: TextStyle(
+                        color: Colors.blue[300],
+                        decoration: TextDecoration.underline,
+                      ),
+                    ),
+                    onTap: () async {
+                      final uri = Uri.parse(useConditionsUrl);
+                      if (await canLaunchUrl(uri)) await launchUrl(uri);
+                    },
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(
+                        Colors.greenAccent,
+                      ),
+                    ),
+                    child: Text(
+                      AppLocalizations.of(context)!.buttonAccept,
+                      style: const TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          );
+        });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(AppLocalizations.of(context)!.homeTitle),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: const Center(
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              'Home page',
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
