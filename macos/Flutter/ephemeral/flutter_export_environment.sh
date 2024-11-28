8d72046eab7227be38c7a227c7a1947d1f6368d3
laurent bernabé <laurent.bernabe@gmail.com>
2024-11-28 12:13:59 +0100

    migrated android setup
    but app crashes when trying to launch stockfish plugin

---
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/home/laurent/Programmes/Programmation/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/laurent/Documents/GitHub/basic_chess_endgames"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=4.1.4"
+export "FLUTTER_BUILD_NUMBER=17"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
